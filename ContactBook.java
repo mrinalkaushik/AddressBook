ab7fdfc487b7d1dc32ca85843a59e33cfa4b25a8
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-22 19:46:41 +0530

    ADD: welcome to address book

---
+++ ContactBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Addressbook;
+import java.util.Scanner;
+public class ContactBook {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("welcome to Address Book System ");
+	 }
+
+}
